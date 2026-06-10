92064a2af7f8a186379dc65442a457b625bb777a
root <root@ip-172-31-20-42.eu-central-1.compute.internal>
2026-06-10 07:14:51 +0000

    Add ROS2 source packages from ros2_ws/src

---
+++ src/turtlebot3_obstacle_avoidance/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'turtlebot3_obstacle_avoidance'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),  
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')), 
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='services_and_actions@local',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'obstacle_avoidance_node = turtlebot3_obstacle_avoidance.obstacle_avoidance_node:main',
+        ],
+    },
+)
